c6ebad1141c401802fed06a29332e7d3f4916b3c
Grinko Sergey <grinya.0895@gmail.com>
2023-11-15 21:10:59 +0200

    added block nav,all sections

---
+++ src/screens/home/HelpSection/help.tsx
@@ -0,0 +1,14 @@
+import style from '../StyleHelpPage.module.scss';
+// import script from '../ScriptPageHelp.module'
+export const Help = () => {
+    return(
+        <section className={style['block-help']}id="block-hepl">
+            <div className={style["block-help-header"]}>
+                <h2 className={style["block-help-header__title"]}>Допомога</h2>
+                <p className={style["block-help-header__sibtitle"]}>Активні напрямки допомоги</p>
+            </div>
+            <hr/>
+            <div className={`${style['block-help-cards']} ${style.flex}`} id="block-help-cards"></div>
+        </section>
+    )
+}
