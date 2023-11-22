94ea0054311080ffdc4751742a345963043ba79b
07nazar <ecsta3452@gmail.com>
2023-11-22 15:51:15 +0200

    added fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,34 @@
+import '@/styles/reset.scss';
 import '@/styles/globals.scss';
 
+import classNames from 'classnames';
+import { Inter, Montserrat_Alternates } from 'next/font/google';
 import { ReactNode } from 'react';
 
-import { Header } from '../components/layout/header';
+import { Header } from '@/components/layout/header';
+
+const inter = Inter({
+  subsets: ['latin', 'cyrillic'],
+  weight: ['400', '500'],
+  display: 'swap',
+  variable: '--fontInter',
+});
+const montserratAlternates = Montserrat_Alternates({
+  subsets: ['latin', 'cyrillic'],
+  weight: ['400', '500', '600', '700'],
+  display: 'fallback',
+  variable: '--fontMontserratAlternates',
+});
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang={'en'}>
-      <body className={'layoutCss'}>
+      <body
+        className={classNames(
+          'layoutCss',
+          inter.variable,
+          montserratAlternates.variable
+        )}>
         <Header />
         <main>{children}</main>
       </body>
