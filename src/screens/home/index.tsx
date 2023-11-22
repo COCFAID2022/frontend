0d9cc124d7d63bc36580cabf54991aac78d536b9
07nazar <ecsta3452@gmail.com>
2023-11-22 14:05:29 +0200

    home screen

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,9 +1,11 @@
-import { Hero } from '@/screens/home/hero';
+import { Hero } from '@/components/hero';
+import { Help } from '@/screens/home/help';
 import { Partners } from '@/screens/home/partners';
 
 export const HomeScreen = () => (
   <>
     <Hero />
     <Partners />
+    <Help />
   </>
 );
