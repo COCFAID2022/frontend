a9cdd00cb5395366d59ee07c486dff5e139ee886
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-11-27 18:35:18 +0200

    add section question
    1) add section question ,
    2) add styles.
    3)  add 2 icons plus and minus,
    4) add two colors
    5) fix reset.scss
    6) add temperary array for question

--- src/components/screens/home/index.tsx
+++ src/components/screens/home/index.tsx
@@ -3,11 +3,14 @@ import { Help } from '@/components/screens/home/help';
 import { OurProjects } from '@/components/screens/home/our-projects';
 import { Partners } from '@/components/screens/home/partners';
 
+import { Question } from './question-section/question';
+
 export const HomeScreen = () => (
   <>
     <Hero />
     <Partners />
     <Help />
     <OurProjects />
+    <Question />
   </>
 );
--- src/components/screens/home/question-section/question.tsx
+++ src/components/screens/home/question-section/question.tsx
@@ -1,20 +1,64 @@
-import MainStyle from '../StyleHelpPage.module.scss';
-import style from './question.module.scss';
+'use client';
 
-export const Question = () => (
-  <section
-    className={`${style['block-news']} ${style['block-question']}`}
-    id={'block-hepl'}>
-    <div className={style['block-help-header']}>
-      <h2 className={style['block-help-header__title']}>Наші проекти</h2>
-      <p className={style['block-help-header__sibtitle']}>
-        Активні напрямки допомоги
-      </p>
-    </div>
-    <hr />
-    <div
-      className={`${style['block-help-cards']} ${MainStyle.flex}`}
-      id={'block-news-cards'}
-    />
-  </section>
-);
+import classNames from 'classnames';
+import Image from 'next/image';
+import { useState } from 'react';
+
+import { Title } from '@/components/common/title';
+import { Typography } from '@/components/ui/typography';
+
+import minus from '../../../../../public/static/icons/minus.svg';
+import plus from '../../../../../public/static/icons/plus.svg';
+import style from './question.module.scss';
+import { questionItem } from './question-item';
+
+export const Question = () => {
+  const [expandedItem, setExpandedItem] = useState<number | null>(null);
+
+  const clickHandler = (index: number) => {
+    if (index === expandedItem) {
+      setExpandedItem(null);
+    } else {
+      setExpandedItem(index);
+    }
+  };
+
+  return (
+    <section className={classNames('container')}>
+      <div className={style.container}>
+        <Title href={'/help'} text={'Питання'} className={style.title} />
+        <Typography
+          fontWeight={'regular'}
+          element={'h5'}
+          className={style.marginHr}>
+          Головні питання
+        </Typography>
+      </div>
+      <ul className={style.list}>
+        {questionItem.map((item, index) => (
+          <li className={style.item} key={item.id}>
+            <button
+              type={'button'}
+              className={style.question}
+              onClick={() => clickHandler(index)}>
+              {item.question}
+              {index === expandedItem ? (
+                <Image src={minus} alt={'minus'} />
+              ) : (
+                <Image src={plus} alt={'plus'} />
+              )}
+            </button>
+            <div
+              className={
+                index === expandedItem
+                  ? classNames(style.accordion, style.open)
+                  : classNames(style.accordion)
+              }>
+              <p>{item.answer}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
