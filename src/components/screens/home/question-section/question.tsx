70193dbaac7d4a296a453b134c90ded9794258ef
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-11-28 16:55:52 +0200

    done section question
    1) update style
    2) add styles to mobile and tablet
    3) add container for consultation
    4) update function clickHandler , for open more than 1 element in accardion

--- src/components/screens/home/question-section/question.tsx
+++ src/components/screens/home/question-section/question.tsx
@@ -5,21 +5,26 @@ import Image from 'next/image';
 import { useState } from 'react';
 
 import { Title } from '@/components/common/title';
+import { Button } from '@/components/ui/button';
 import { Typography } from '@/components/ui/typography';
+import arrowTopRightLight from '@/icons/arrowTopRightLight.svg';
+import minus from '@/icons/minus.svg';
+import phoneIcon from '@/icons/phone.svg';
+import plus from '@/icons/plus.svg';
 
-import minus from '../../../../../public/static/icons/minus.svg';
-import plus from '../../../../../public/static/icons/plus.svg';
 import style from './question.module.scss';
 import { questionItem } from './question-item';
 
 export const Question = () => {
-  const [expandedItem, setExpandedItem] = useState<number | null>(null);
+  const [expandedItems, setExpandedItems] = useState<number[]>([]);
 
   const clickHandler = (index: number) => {
-    if (index === expandedItem) {
-      setExpandedItem(null);
+    if (expandedItems.includes(index)) {
+      // Якщо елемент вже розгорнуто, видаляємо його із списку розгорнутих
+      setExpandedItems(expandedItems.filter(item => item !== index));
     } else {
-      setExpandedItem(index);
+      // Якщо елемент не розгорнуто, додаємо його до списку розгорнутих
+      setExpandedItems([...expandedItems, index]);
     }
   };
 
@@ -42,23 +47,64 @@ export const Question = () => {
               className={style.question}
               onClick={() => clickHandler(index)}>
               {item.question}
-              {index === expandedItem ? (
-                <Image src={minus} alt={'minus'} />
-              ) : (
-                <Image src={plus} alt={'plus'} />
-              )}
+              <Image
+                src={expandedItems.includes(index) ? minus : plus}
+                alt={expandedItems.includes(index) ? 'minus' : 'plus'}
+              />
             </button>
             <div
-              className={
-                index === expandedItem
-                  ? classNames(style.accordion, style.open)
-                  : classNames(style.accordion)
-              }>
+              className={style.accordion}
+              style={{
+                maxHeight: expandedItems.includes(index) ? '1000px' : '0',
+              }}>
               <p>{item.answer}</p>
             </div>
           </li>
         ))}
       </ul>
+
+      <div className={style.consultation}>
+        <div className={style.consultationText}>
+          <Typography
+            fontWeight={'semiBold'}
+            element={'h3'}
+            className={style.titleHelp}>
+            Потрібна консультація?
+          </Typography>
+          <Typography fontWeight={'regular'} element={'p'}>
+            Наші менеджери завжди раді Вам допомогти
+          </Typography>
+        </div>
+        <div className={style.btnHelp}>
+          <Button
+            variant={'outlined'}
+            size={'sm'}
+            icon={{ variant: 'blue', position: 'left', image: phoneIcon }}
+            label={'Гаряча лінія'}
+            value={{
+              element: 'h7',
+              fontWeight: 'semiBold',
+              color: 'blue',
+              text: '050 612 45 68',
+            }}
+          />
+          <Button
+            variant={'outlined'}
+            size={'sm'}
+            icon={{
+              variant: 'blue',
+              position: 'left',
+              image: arrowTopRightLight,
+            }}
+            value={{
+              text: 'Підтримати',
+              color: 'blue',
+              fontWeight: 'semiBold',
+              element: 'h7',
+            }}
+          />
+        </div>
+      </div>
     </section>
   );
 };
