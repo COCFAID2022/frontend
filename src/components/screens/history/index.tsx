5fb05d6adac508b0ccd4314a45a705f2a8778c08
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-12-01 12:18:59 +0200

    Do section Header in historyScreen
    1) add icon heard (Vector)
    2) create component VideoPlayer
    3) create component Header
    4) add style for mobile, tablet and desktop
    ___________
    Warning - Video player is temperary , we have change it for better one

--- src/components/screens/history/index.tsx
+++ src/components/screens/history/index.tsx
@@ -1,9 +1,11 @@
 import { Descriprion } from './discription';
 import { Feedback } from './feedback';
+import { Header } from './header';
 
 export const HistoryScreen = () => {
   return (
     <>
+      <Header />
       <Descriprion />
       <Feedback />
     </>
