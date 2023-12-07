b44b0839676fee2474a657fa17a4b0d8ff5b3dab
07nazar <ecsta3452@gmail.com>
2023-12-07 15:41:55 +0200

    merge

---
+++ src/components/screens/selected-history-page/index.tsx
@@ -0,0 +1,15 @@
+import { Descriprion } from './discription';
+import { Feedback } from './feedback';
+import { Header } from './header';
+import { OtherHistory } from './other-history';
+
+export const SelectedHistoryScreen = () => {
+  return (
+    <>
+      <Header />
+      <Descriprion />
+      <Feedback />
+      <OtherHistory />
+    </>
+  );
+};
